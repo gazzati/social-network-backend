12d415786d90b93d0be2aca431cd8813ae45f811
gazzaevti <gazzaevtimur@gmail.com>
2021-02-06 00:07:13 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -29,15 +29,7 @@ mongoose.connect(
 app.use(express.json())
 app.use('/uploads', express.static('src/uploads'))
 app.use(cookieParser())
-//app.use(cors({ origin: true, credentials: true }))
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*')
-    res.header("Access-Control-Allow-Credentials", true)
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next()
-})
+app.use(cors({ origin: true, credentials: true }))
 
 //Route Middlewares
 app.use('/api/auth', authRoute)

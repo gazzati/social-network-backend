2a24a6c534b01146fa353796209fde2b1ab25c00
gazzati <gazzaevtimur@gmail.com>
2021-04-02 19:57:11 +0300

    change port

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    PORT: 8000,
-    SOCKET_PORT: 8001,
+    PORT: process.env.PORT || 8000,
+    SOCKET_PORT: process.env.PORT || 8001,
     DB_CONNECT: 'mongodb+srv://gazzaevtimur:timur99@cluster0.xbdsh.mongodb.net/social-network?retryWrites=true&w=majority',
     TOKEN_SECRET: 'riopklmbascadc'
 }
